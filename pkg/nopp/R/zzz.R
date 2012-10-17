91d9a66065d5fa5e3add501204d0799f94ba89d1
iacus <iacus@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-10-17 10:30:16 +0000

    release 1
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nopp@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/nopp/R/zzz.R
@@ -0,0 +1,7 @@
+.onAttach <- function(libname, pkgname) {
+    NOPPver <- read.dcf(file=system.file("DESCRIPTION", package=pkgname),
+                      fields="Version")
+    packageStartupMessage(paste(pkgname, NOPPver))
+    packageStartupMessage("Type noppNews() to see new features/changes/bug fixes.")
+    packageStartupMessage('Type vignette("nopp") to read the vignette')
+}
